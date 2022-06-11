a27f2277921d0f6d3b6439f41213a7b5b9d05b4e
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-06-11 15:42:31 +0200

    page

--- first/index.py
+++ first/index.py
@@ -1,3 +1,14 @@
 x = 11
+listExample = [1,2,3,4,'jkjkjk',"unun"]
 
-print( type(x) )
+if x > 1: 
+    print(x) 
+else:
+    print("false")     
+
+def first(x, y): 
+    return x + y
+
+print(first(1,2))
+
+print(listExample[1:3])
